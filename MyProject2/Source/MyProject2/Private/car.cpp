7054ed96223e8908a5e2b39d42ec47d176169ecb
barathdandu <barathdandu01@gmail.com>
2020-07-01 11:53:50 +0530

    Car Moving Forward and Back with PhysicsConstraints. Added Camera

---
+++ MyProject2/Source/MyProject2/Private/car.cpp
@@ -0,0 +1,131 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "car.h"
+
+// Sets default values
+Acar::Acar()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(FName("Body"));
+	RootComponent = Body;
+
+	RR = CreateDefaultSubobject<USceneComponent>(FName("RR"));
+	RR->SetupAttachment(Body);
+
+	SceneAxleConstraintRR = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("SceneAxleConstraintRR"));
+	SceneAxleConstraintRR->SetupAttachment(RR);
+
+	AxleRR = CreateDefaultSubobject<USphereComponent>(FName("AxleRR"));
+	AxleRR->SetupAttachment(RR);
+
+	AxleWheelConstraintRR = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("AxleWheelConstraintRR"));
+	AxleWheelConstraintRR->SetupAttachment(AxleRR);
+
+	WheelRR = CreateDefaultSubobject<UStaticMeshComponent>(FName("WheelRR"));
+	WheelRR->SetupAttachment(AxleRR);
+
+	RL = CreateDefaultSubobject<USceneComponent>(FName("RL"));
+	RL->SetupAttachment(Body);
+
+	SceneAxleConstraintRL = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("SceneAxleConstraintRL"));
+	SceneAxleConstraintRL->SetupAttachment(RL);
+
+	AxleRL = CreateDefaultSubobject<USphereComponent>(FName("AxleRL"));
+	AxleRL->SetupAttachment(RL);
+
+	AxleWheelConstraintRL = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("AxleWheelConstraintRL"));
+	AxleWheelConstraintRL->SetupAttachment(AxleRL);
+
+	WheelRL = CreateDefaultSubobject<UStaticMeshComponent>(FName("WheelRL"));
+	WheelRL->SetupAttachment(AxleRL);
+
+	FR = CreateDefaultSubobject<USceneComponent>(FName("FR"));
+	FR->SetupAttachment(Body);
+
+	SceneAxleConstraintFR = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("SceneAxleConstraintFR"));
+	SceneAxleConstraintFR->SetupAttachment(FR);
+
+	AxleFR = CreateDefaultSubobject<USphereComponent>(FName("AxleFR"));
+	AxleFR->SetupAttachment(FR);
+
+	AxleWheelConstraintFR = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("AxleWheelConstraintFR"));
+	AxleWheelConstraintFR->SetupAttachment(AxleFR);
+
+	WheelFR = CreateDefaultSubobject<UStaticMeshComponent>(FName("WheelFR"));
+	WheelFR->SetupAttachment(AxleFR);
+
+	FL = CreateDefaultSubobject<USceneComponent>(FName("FL"));
+	FL->SetupAttachment(Body);
+
+	SceneAxleConstraintFL = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("SceneAxleConstraintFL"));
+	SceneAxleConstraintFL->SetupAttachment(FL);
+
+	AxleFL = CreateDefaultSubobject<USphereComponent>(FName("AxleFL"));
+	AxleFL->SetupAttachment(FL);
+
+	AxleWheelConstraintFL = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("AxleWheelConstraintFL"));
+	AxleWheelConstraintFL->SetupAttachment(AxleFL);
+
+	WheelFL = CreateDefaultSubobject<UStaticMeshComponent>(FName("WheelFL"));
+	WheelFL->SetupAttachment(AxleFL);
+
+
+}
+
+// Called when the game starts or when spawned
+void Acar::BeginPlay()
+{
+	Super::BeginPlay();
+
+	RR->SetRelativeLocation(RRLocation);
+
+	SetupConstraint();
+	
+}
+
+void Acar::SetupConstraint()
+{
+	SceneAxleConstraintRR->SetConstrainedComponents(Body, NAME_None, AxleRR, NAME_None);
+	AxleWheelConstraintRR->SetConstrainedComponents(AxleRR, NAME_None, WheelRR, NAME_None);
+
+	SceneAxleConstraintRL->SetConstrainedComponents(Body, NAME_None, AxleRL, NAME_None);
+	AxleWheelConstraintRL->SetConstrainedComponents(AxleRL, NAME_None, WheelRL, NAME_None);
+
+	SceneAxleConstraintFR->SetConstrainedComponents(Body, NAME_None, AxleFR, NAME_None);
+	AxleWheelConstraintFR->SetConstrainedComponents(AxleFR, NAME_None, WheelFR, NAME_None);
+
+	SceneAxleConstraintFL->SetConstrainedComponents(Body, NAME_None, AxleFL, NAME_None);
+	AxleWheelConstraintFL->SetConstrainedComponents(AxleFL, NAME_None, WheelFL, NAME_None);
+}
+
+void Acar::AddDrivingForce(float Force)
+{
+	float MultipliedForce = Force * MaxDrivingForce;
+
+	UE_LOG(LogTemp, Warning, TEXT("%f"), MultipliedForce);
+	WheelRR->AddForce(AxleFL->GetRightVector() * MultipliedForce);
+	WheelRL->AddForce(AxleFL->GetRightVector() * MultipliedForce);
+	WheelFR->AddForce(AxleFL->GetRightVector() * MultipliedForce);
+	WheelFL->AddForce(AxleFL->GetRightVector() * MultipliedForce);
+}
+
+// Called every frame
+void Acar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+
+
+}
+
+// Called to bind functionality to input
+void Acar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ MyProject2/Source/MyProject2/Public/car.h
@@ -0,0 +1,111 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "car.generated.h"
+
+UCLASS()
+class MYPROJECT2_API Acar : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	Acar();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:	
+	void SetupConstraint();
+
+
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* Body= nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USceneComponent* RR = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* SceneAxleConstraintRR = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USphereComponent * AxleRR = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* AxleWheelConstraintRR = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* WheelRR = nullptr;
+
+
+
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USceneComponent* RL = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* SceneAxleConstraintRL = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USphereComponent * AxleRL = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* AxleWheelConstraintRL = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* WheelRL = nullptr;
+
+
+
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USceneComponent* FR = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* SceneAxleConstraintFR = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USphereComponent * AxleFR = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* AxleWheelConstraintFR = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* WheelFR = nullptr;
+
+
+
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USceneComponent* FL = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* SceneAxleConstraintFL = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USphereComponent * AxleFL = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* AxleWheelConstraintFL = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* WheelFL = nullptr;
+
+
+
+
+	UPROPERTY(EditAnywhere, Category = "Location")
+		FVector RRLocation = FVector(50.f,50.f,0.f);
+	UPROPERTY(EditAnywhere, Category = "Location")
+		FVector RLLocation;
+	UPROPERTY(EditAnywhere, Category = "Location")
+		FVector FRLocation; 
+	UPROPERTY(EditAnywhere, Category = "Location")
+		FVector FLLocation;	
+
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void AddDrivingForce(float Force);
+
+	UPROPERTY(EditDefaultsOnly)
+	float MaxDrivingForce = 40000;
+};
