bbd2c8a2ea2175faf59bf2054161aae2499be3be
barathdandu <barathdandu01@gmail.com>
2020-07-16 20:05:45 +0530

    Long slip and lateral slip calculated and Fx and Fy forces depend on them , car now moves when put into gear

--- MyProject2/Source/MyProject2/Private/Imprezza.cpp
+++ MyProject2/Source/MyProject2/Private/Imprezza.cpp
@@ -11,7 +11,7 @@
 // Sets default values
 AImprezza::AImprezza()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	Body = CreateDefaultSubobject<UStaticMeshComponent>(FName("Body"));
@@ -32,13 +32,13 @@ AImprezza::AImprezza()
 
 	TopLinkFL = CreateDefaultSubobject<USceneComponent>(FName("TopLinkFL"));
 	TopLinkFL->AttachToComponent(Body, FAttachmentTransformRules::SnapToTargetNotIncludingScale, "FL");
-	
+
 	TopLinkFR = CreateDefaultSubobject<USceneComponent>(FName("TopLinkFR"));
 	TopLinkFR->AttachToComponent(Body, FAttachmentTransformRules::SnapToTargetNotIncludingScale, "FR");
-	
+
 	TopLinkRL = CreateDefaultSubobject<USceneComponent>(FName("TopLinkRL"));
 	TopLinkRL->AttachToComponent(Body, FAttachmentTransformRules::SnapToTargetNotIncludingScale, "RL");
-	
+
 	TopLinkRR = CreateDefaultSubobject<USceneComponent>(FName("TopLinkRR"));
 	TopLinkRR->AttachToComponent(Body, FAttachmentTransformRules::SnapToTargetNotIncludingScale, "RR");
 
@@ -51,11 +51,11 @@ AImprezza::AImprezza()
 		WheelFL->SetStaticMesh(WheelMesh.Object);
 		WheelFL->SetRelativeLocation(FVector(0.f, 0.f, -50.f));
 		WheelFL->SetWorldScale3D(FVector(1.f, 1.f, 1.f));
-		WheelFL->SetRelativeRotation(FRotator(0.f,0.f,180.f));
+		WheelFL->SetRelativeRotation(FRotator(0.f, 0.f, 180.f));
 		WheelFL->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
-		
+
 	}
-	
+
 	WheelFR = CreateDefaultSubobject<UStaticMeshComponent>(FName("WheelFR"));
 	WheelFR->AttachToComponent(TopLinkFR, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
 	if (WheelMesh.Succeeded())
@@ -112,7 +112,7 @@ AImprezza::AImprezza()
 	SpringArm->bUsePawnControlRotation = true;
 	SpringArm->SetupAttachment(RootComponent);
 	SpringArm->TargetArmLength = 400.0f;
-	SpringArm->bEnableCameraLag = true;
+	SpringArm->bEnableCameraLag = false;
 	SpringArm->bEnableCameraRotationLag = true;
 	SpringArm->bInheritPitch = true;
 	SpringArm->bInheritYaw = true;
@@ -126,17 +126,22 @@ AImprezza::AImprezza()
 	Camera->bUsePawnControlRotation = false;
 	Camera->FieldOfView = 90.f;
 
-	
+
 	TopLinkComponent = { TopLinkFL,TopLinkFR,TopLinkRL,TopLinkRR };
 
 	WheelComponent = { WheelFL, WheelFR, WheelRL, WheelRR };
 
-	FWheelStruct WSFL  ;
-	FWheelStruct WSFR ;
-	FWheelStruct WSRL ;
-	FWheelStruct WSRR ;
+	FWheelStruct WSFL;
+	FWheelStruct WSFR;
+	FWheelStruct WSRL;
+	FWheelStruct WSRR;
 
-	Wheel = { WSFL,WSFR,WSRL,WSRR };
+	Wheel = { WSFL,WSFL,WSFL,WSFL };
+
+	Wheel[0].CorneringStiffness = 0.7f;
+	Wheel[1].CorneringStiffness = 0.7f;
+	Wheel[2].CorneringStiffness = 0.5f;
+	Wheel[3].CorneringStiffness = 0.5f;
 
 	UE_LOG(LogTemp, Warning, TEXT("radius %f, mass = %f"), WSFL.Radius, WSFL.Mass);
 
@@ -147,9 +152,9 @@ AImprezza::AImprezza()
 
 	Suspension = { SSFL,SSFR ,SSRL,SSRR };
 
-	UE_LOG(LogTemp, Warning, TEXT("length %f, Travel = %f, Stiffness =%f, Damper =%f, Min = %f, Max = %f"), SSFL.Rest_Length, SSFL.Travel, SSFL.Stiffness, SSFL.Damper,SSFL.Force_Min,SSFL.Force_Max);
-	
-	bool WCFL  = false;
+	UE_LOG(LogTemp, Warning, TEXT("length %f, Travel = %f, Stiffness =%f, Damper =%f, Min = %f, Max = %f"), SSFL.Rest_Length, SSFL.Travel, SSFL.Stiffness, SSFL.Damper, SSFL.Force_Min, SSFL.Force_Max);
+
+	bool WCFL = false;
 	bool WCFR = false;
 	bool WCRL = false;
 	bool WCRR = false;
@@ -163,23 +168,98 @@ AImprezza::AImprezza()
 	FHitResult OUTHitResultRL;
 	FHitResult OUTHitResultRR;
 
-	ArrayHitResult = { OUTHitResultFL ,OUTHitResultFR, OUTHitResultRL,	OUTHitResultRR};
-	
+	ArrayHitResult = { OUTHitResultFL ,OUTHitResultFR, OUTHitResultRL,	OUTHitResultRR };
+
 	UE_LOG(LogTemp, Warning, TEXT("ArrayHitResult Array Length %i"), ArrayHitResult.Num());
 
-	FVector WLVLFL = FVector(0.0, 0.0f, 0.0f);
-	FVector WLVLFR = FVector(0.0, 0.0f, 0.0f);
-	FVector WLVLRL = FVector(0.0, 0.0f, 0.0f);
-	FVector WLVLRR = FVector(0.0, 0.0f, 0.0f);
+	FVector WLVLFL;
+	FVector WLVLFR;
+	FVector WLVLRL;
+	FVector WLVLRR;
 
 	WheelLinearVelocityLocal = { WLVLFL,WLVLFR,WLVLRL,WLVLRR };
 
+	TC1 = FKeyHandle();
+	TC2 = FKeyHandle();
+	TC3 = FKeyHandle();
+
+	TorqueCurve.EditorCurveData.AddKey(0.f, 0.f, false, TC1);
+	TorqueCurve.EditorCurveData.AddKey(4500.f, 200.f, false, TC2);
+	TorqueCurve.EditorCurveData.AddKey(9000.f, 105.f, false, TC3);
+
+	TorqueCurve.EditorCurveData.SetKeyTangentMode(TC1, ERichCurveTangentMode::RCTM_Auto);
+	TorqueCurve.EditorCurveData.SetKeyInterpMode(TC1, ERichCurveInterpMode::RCIM_Cubic);
+
+	TorqueCurve.EditorCurveData.SetKeyTangentMode(TC2, ERichCurveTangentMode::RCTM_Auto);
+	TorqueCurve.EditorCurveData.SetKeyInterpMode(TC2, ERichCurveInterpMode::RCIM_Cubic);
+
+	TorqueCurve.EditorCurveData.SetKeyTangentMode(TC3, ERichCurveTangentMode::RCTM_Auto);
+	TorqueCurve.EditorCurveData.SetKeyInterpMode(TC3, ERichCurveInterpMode::RCIM_Cubic);
+
+	FC1 = FKeyHandle();
+	FC2 = FKeyHandle();
+	FC3 = FKeyHandle();
+
+	ForceCurve.EditorCurveData.AddKey(0.f, 0.f, false, FC1);
+	ForceCurve.EditorCurveData.AddKey(1.f, 1.f, false, FC2);
+	ForceCurve.EditorCurveData.AddKey(1.6f, 0.7f, false, FC3);
+
+	ForceCurve.EditorCurveData.SetKeyTangentMode(FC1, ERichCurveTangentMode::RCTM_Auto);
+	ForceCurve.EditorCurveData.SetKeyInterpMode(FC1, ERichCurveInterpMode::RCIM_Cubic);
+
+	ForceCurve.EditorCurveData.SetKeyTangentMode(FC2, ERichCurveTangentMode::RCTM_Auto);
+	ForceCurve.EditorCurveData.SetKeyInterpMode(FC2, ERichCurveInterpMode::RCIM_Cubic);
+
+	ForceCurve.EditorCurveData.SetKeyTangentMode(FC3, ERichCurveTangentMode::RCTM_Auto);
+	ForceCurve.EditorCurveData.SetKeyInterpMode(FC3, ERichCurveInterpMode::RCIM_Cubic);
+
+	float GearR = -3.615;
+	float GearN = 0;
+	float Gear1 = 3.583;
+	float Gear2 = 2.038;
+	float Gear3 = 1.414;
+	float Gear4 = 1.108;
+	float Gear5 = 0.878;
+
+	GearRatio = { GearR, GearN ,Gear1 ,Gear2 ,Gear3 ,Gear4 ,Gear5 };
+
+	UE_LOG(LogTemp, Warning, TEXT("GearRatio Array Length %i"), GearRatio.Num());
+
+	DriveTorque = { 0.f, 0.f, 0.f, 0.f };
+
+	WheelInertia = { 0.f, 0.f, 0.f, 0.f };
+
+	WheelAngularVelocity = { 0.f, 0.f, 0.f, 0.f };
+
+	LongSlipVelocity = { 0.f, 0.f, 0.f, 0.f };
+
+	UE_LOG(LogTemp, Warning, TEXT("DriveTorque Array Length %i"), DriveTorque.Num());
+
+	LongLateralSlipVector = FVector2D(LongSlipNormalized, LateralSlipNormalized);
+
+	TireForce = FVector2D(FxVal, FyVal);
 }
 
 // Called when the game starts or when spawned
 void AImprezza::BeginPlay()
 {
 	Super::BeginPlay();
+
+	RPM_to_RAD_PS = (2 * PI) / 60;
+	RAD_PS_to_RPM = 1 / RPM_to_RAD_PS;
+
+	if (DriveType == EDriveType::FWD)
+	{
+		TorqueRatio = { 1.f , 1.f, 0.f, 0.f };
+	}
+	else if (DriveType == EDriveType::RWD)
+	{
+		TorqueRatio = { 0.f ,0.f, 1.f, 1.f };
+	}
+	else
+	{
+		TorqueRatio = { FrontBiasPercent , FrontBiasPercent, 1 - FrontBiasPercent,  1 - FrontBiasPercent };
+	}
 
 	float CurrentRayLength;
 
@@ -191,9 +271,7 @@ void AImprezza::BeginPlay()
 		CurrentRayLength = Wheel[i].Radius + Suspension[i].Rest_Length + Suspension[i].Travel;
 		RayLength.Add(CurrentRayLength);
 
-
 		CurrentRayLength = 0;
-
 
 		CurrentLength = Suspension[i].Rest_Length;
 		Length.Add(CurrentLength);
@@ -204,11 +282,18 @@ void AImprezza::BeginPlay()
 		Fx.Add(CurrentLength);
 		Fy.Add(CurrentLength);
 		Fz.Add(CurrentLength);
+
+		WheelInertia[i] = FMath::Square(Wheel[i].Radius / 100) * Wheel[i].Mass * 0.5f;
+
+		UE_LOG(LogTemp, Warning, TEXT("Torque Bias: %f"), TorqueRatio[i]);
+
 	}
+	
 	UE_LOG(LogTemp, Warning, TEXT("RayLength Array Length %i"), RayLength.Num());
 	UE_LOG(LogTemp, Warning, TEXT("Length Array Length %i"), Length.Num());
 	UE_LOG(LogTemp, Warning, TEXT("Fz Array Length %i"), Fz.Num());
 	UE_LOG(LogTemp, Warning, TEXT("LastLength Array Length %i"), LastLength.Num());
+
 }
 
 // Called every frame
@@ -216,44 +301,76 @@ void AImprezza::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	
+	DeltaTimee = DeltaTime;
+
 	for (int32 i = 0; i < 4; i++)
 	{
+		DriveTorque[i] = FMath::Max(EngineTorque, 0.f) * TotalGearRatio * TorqueRatio[i] * 0.5;
+
+		//UE_LOG(LogTemp, Warning, TEXT("DriveTorque on Wheel %i is %f"), i, DriveTorque[i]);
+
+		//AngularVelocity += AngularAcceleration*DeltaTime
+		//AngularAcceleration = Torque/inertia
+		//max wheel speed on currentgear;
 		
+		if (Gear != 1)
+		{
+			MaxWheelSpeedOnCurrentGear = FMath::Abs(EngineAngularVelocity / TotalGearRatio);
+		}
+		else
+		{
+			MaxWheelSpeedOnCurrentGear = 9999.f;
+		}
+
+		WheelAngularVelocity[i] = FMath::Sign(MaxWheelSpeedOnCurrentGear) * FMath::Min(
+			FMath::Abs(WheelAngularVelocity[i] + ((DriveTorque[i] / WheelInertia[i]) * DeltaTime)),
+			MaxWheelSpeedOnCurrentGear);
+
+		//UE_LOG(LogTemp, Warning, TEXT("WheelAngularVelocity %f"), WheelAngularVelocity[i]*RAD_PS_to_RPM);
+		
+		//max wheel speed on currentgear;
+	
+
+
 		auto TopLink = TopLinkComponent[i];
 		if (!TopLink) { UE_LOG(LogTemp, Warning, TEXT("TopLink Error")); return; }
 		auto StartLocation = TopLink->GetComponentLocation();
+
 		auto gUpVector = TopLink->GetUpVector();
-		auto UpVector = (gUpVector) * RayLength[i];
-		auto EndLocation =  StartLocation - UpVector;
+		auto gForwardVector = TopLink->GetForwardVector();
+		auto gRightVector = TopLink->GetRightVector();
+		auto gWheelCompLoc = WheelComponent[i]->GetComponentLocation();
+
+		auto UpVector = (gUpVector)* RayLength[i];
+		auto EndLocation = StartLocation - UpVector;
 
 		FCollisionQueryParams TraceParams;
-		
+
 
 		//UE_LOG(LogTemp, Warning, TEXT("%i, %s"),i,*StartLocation.ToString());
 
 		bool bWheelContact = GetWorld()->LineTraceSingleByChannel(ArrayHitResult[i], StartLocation, EndLocation, ECC_Visibility, TraceParams);
-		
+
 		//auto Result = ArrayHitResult[i].Location;
 		//UE_LOG(LogTemp, Warning, TEXT("%s") ,*Result.ToText);
-		
+
 		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 1.f);
 
 		if (bWheelContact)
 		{
-			DrawDebugBox(GetWorld(), ArrayHitResult[i].ImpactPoint, FVector(10.f,10.f,10.f), FColor::Red, false, 1.f);
+			DrawDebugBox(GetWorld(), ArrayHitResult[i].ImpactPoint, FVector(10.f, 10.f, 10.f), FColor::Red, false, 1.f);
 			WheelContact[i] = bWheelContact;
 
-			auto LengthUpVector = (gUpVector) * Wheel[i].Radius;
+			auto LengthUpVector = (gUpVector)* Wheel[i].Radius;
 			auto HitLocxUpVec = LengthUpVector + ArrayHitResult[i].Location;
 			auto VectorLength = FMath::Clamp(
 				StartLocation.Z - HitLocxUpVec.Z, Suspension[i].Rest_Length - Suspension[i].Travel, Suspension[i].Rest_Length + Suspension[i].Travel);
-			
+
 			Length[i] = VectorLength;
 			//UE_LOG(LogTemp, Warning, TEXT("%f") ,Length[i]);
 
 			float SpringForce = (Suspension[i].Stiffness)*(Suspension[i].Rest_Length - Length[i]);
-				
+
 			float DamperForce = (Suspension[i].Damper)*((LastLength[i] - Length[i]) / DeltaTime);
 
 			float  SpringplusDamperForce = FMath::Clamp(SpringForce + DamperForce, Suspension[i].Force_Min, Suspension[i].Force_Max);
@@ -274,39 +391,96 @@ void AImprezza::Tick(float DeltaTime)
 		}
 		else
 		{
-			WheelContact[i]  = false;
+			WheelContact[i] = false;
 			Length[i] = Suspension[i].Rest_Length + Suspension[i].Travel;
 			//UE_LOG(LogTemp, Warning, TEXT("%f"), Length[i]);
 		}
 		//UE_LOG(LogTemp, Warning, TEXT("%i"), WheelContact[i]);
-		 auto Direction = Body->GetPhysicsLinearVelocityAtPoint(
-			(WheelComponent[i]->GetComponentLocation())-(gUpVector*Wheel[i].Radius));
+		auto Direction = Body->GetPhysicsLinearVelocityAtPoint(
+			(gWheelCompLoc)-(gUpVector*Wheel[i].Radius));
 
-		auto ComponentTransform = (TopLinkComponent[i]->GetComponentTransform());
+		auto ComponentTransform = (TopLink->GetComponentTransform());
 		ComponentTransform = Inverse(ComponentTransform);
+
+		WheelLinearVelocityLocal[i] = (FVector(UKismetMathLibrary::TransformDirection(
+			ComponentTransform, (Direction / 100))));
+
+		//UE_LOG(LogTemp, Warning, TEXT("%s"), WheelLinearVelocityLocal[i].ToString());
 		
-		WheelLinearVelocityLocal[i] = FVector(UKismetMathLibrary::TransformDirection(
-			ComponentTransform,(Direction/100))); 
-		
-		UE_LOG(LogTemp, Warning, TEXT("%s"), *WheelLinearVelocityLocal[i].ToString());
+		LongSlipVelocity[i] = (WheelAngularVelocity[i] * (Wheel[i].Radius / 100)) - WheelLinearVelocityLocal[i].X;
+
+		//UE_LOG(LogTemp, Warning, TEXT("LongSlipVelocity %i: %f"),i ,LongSlipVelocity[i]);
+
+
 		if (WheelContact[i])
 		{
-			Fx[i] = Fz[i] * 0.5*ForwardAxisValue;
+			/*Fx[i] = Fz[i] * 0.5 * ForwardAxisValue;
 			Fy[i] = FMath::Clamp(WheelLinearVelocityLocal[i].Y*-1 * Fz[i], -Fz[i], Fz[i]);
 
-			auto AddingForce = 	TopLinkComponent[i]->GetForwardVector() * Fx[i] + TopLinkComponent[i]->GetRightVector() * Fy[i];
+			auto AddingForce = gForwardVector * Fx[i] + gRightVector * Fy[i];
 			Body->AddForceAtLocation(AddingForce * 100, ArrayHitResult[i].Location);
+			*/
+			
+
+			//if(carspeed*slipSpeed>0) =  traction ,else friction
+			LongLateralSlipVector.Y = FMath::Clamp((WheelLinearVelocityLocal[i].Y * Wheel[i].CorneringStiffness * -1.f), -1.f, 1.f);
+			
+			if ((LongSlipVelocity[i] * WheelLinearVelocityLocal[i].X) > 0)
+			{
+				//Traction = Drivetorque/radius
+				LongLateralSlipVector.X = FMath::Clamp( (DriveTorque[i] / (Wheel[i].Radius / 100)) / FMath::Max(Fz[i], 0.000001f),-2.f,2.f );
+			}
+			else
+			{
+				LongLateralSlipVector.X = FMath::Clamp(Wheel[i].LongStiffness * LongSlipVelocity[i], -2.f, 2.f);
+			}
+
+			TireForce = FMath::Max(Fz[i], 0.f) *
+				LongLateralSlipVector.GetSafeNormal() * ForceCurve.EditorCurveData.Eval(LongLateralSlipVector.Size());
+
+			Fx[i] = TireForce.X;
+			Fy[i] = TireForce.Y;
 		}
 		else
 		{
 			Fx[i] = 0;
-			Fy[i] = 0; 
+			Fy[i] = 0;
 		}
-		
+
+		auto AddingForce = gForwardVector * Fx[i] + gRightVector * Fy[i];
+		Body->AddForceAtLocation(AddingForce * 100, ArrayHitResult[i].Location);
+
+		UE_LOG(LogTemp, Warning, TEXT("%i Fx: %f Fy: %f"), i, Fx[i], Fy[i]);
+
+		//wheel rotation
+		if (i == 0 || i == 2) {
+			WheelComponent[i]->AddLocalRotation(
+				FRotator((FMath::RadiansToDegrees(WheelAngularVelocity[i]) * DeltaTime), 0.f, 0.f));
+		}
+		else {
+			WheelComponent[i]->AddLocalRotation(
+				FRotator((FMath::RadiansToDegrees(-WheelAngularVelocity[i]) * DeltaTime), 0.f, 0.f));
+		}
+
+
+		if (DebugForces)
+		{
+			auto DebugLineStart = gWheelCompLoc;
+
+			TArray<FVector>  DebugLineEnd = { (gWheelCompLoc + ((Fx[i] / 35)*(gForwardVector))),
+				(gWheelCompLoc + ((Fy[i] / 35)*(gRightVector))),(gWheelCompLoc + ((Fz[i] / 35)*(gUpVector))) };
+
+			TArray<FColor> DebugColor = { FColor::Red, FColor::Green, FColor::Blue };
+
+			for (int32 j = 0; j < 3; j++)
+			{
+				DrawDebugLine(GetWorld(), DebugLineStart, DebugLineEnd[j], DebugColor[j], false, 0.f, 0, 8.f);
+			}
+		}
 
 	}
-	
 
+	//UE_LOG(LogTemp, Warning, TEXT("Gear: %f, Total Gear Ratio: %f"), GearRatio[Gear], TotalGearRatio);
 }
 
 // Called to bind functionality to input
@@ -317,8 +491,12 @@ void AImprezza::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	PlayerInputComponent->BindAxis("Look_X", this, &AImprezza::LookX);
 	PlayerInputComponent->BindAxis("Look_Y", this, &AImprezza::LookY);
 	PlayerInputComponent->BindAxis("SteerR", this, &AImprezza::Steer);
+	PlayerInputComponent->BindAxis("Forward", this, &AImprezza::Forward);
 	PlayerInputComponent->BindAxis("Throttle", this, &AImprezza::Throttle);
-	
+
+	PlayerInputComponent->BindAction("BodyVisibility", IE_Pressed, this, &AImprezza::SetBodyVisibility);
+	PlayerInputComponent->BindAction("P", IE_Pressed, this, &AImprezza::GearUp);
+	PlayerInputComponent->BindAction("L", IE_Pressed, this, &AImprezza::GearDown);
 }
 
 void AImprezza::LookX(float Value)
@@ -337,19 +515,52 @@ void AImprezza::LookY(float Value)
 	}
 }
 
-
 void AImprezza::Throttle(float Value)
 {
+	ThrottleFilter = false;
+
+	if (ThrottleFilter == false)
+	{
+		ThrottleValue = Value;
+	}
+	else
+	{
+		if (Value != 0)
+		{
+			ThrottleValue = FMath::Min((DeltaTimee * 1 + ThrottleValue), 1.f);
+		}
+		else
+		{
+			ThrottleValue = FMath::Max((ThrottleValue - DeltaTimee * 2), 0.f);
+		}
+	}
+
+
+	EngineTorque = FMath::Lerp(Engine.BackTorque, ThrottleValue * TorqueCurve.EditorCurveData.Eval(EngineRPM), ThrottleValue);
+
+	EngineAngularVelocity = FMath::Clamp(
+		(EngineAngularVelocity + DeltaTimee * (EngineTorque / Engine.Inertia)),
+		(RPM_to_RAD_PS * Engine.IdleRPM),
+		(Engine.MaxRPM * RPM_to_RAD_PS));
+
+	EngineRPM = EngineAngularVelocity * RAD_PS_to_RPM;
+
+	//UE_LOG(LogTemp, Warning, TEXT("EngineRPM :%f, EngineTorque: %f "), EngineRPM, EngineTorque);
+}
+
+void AImprezza::Forward(float Value)
+{
 	ForwardAxisValue = Value;
-	
+
 	//UE_LOG(LogTemp, Warning, TEXT("Throttle %f"), Value);
 }
 
+
 void AImprezza::Steer(float Value)
 {
-	if (Value != 0) 
+	if (Value != 0)
 	{
-		SteeringAngle = FMath::Clamp(((Value * 2.f) + SteeringAngle), -SteeringAngleMax, SteeringAngleMax);
+		SteeringAngle = FMath::Clamp(((Value * 125 * DeltaTimee) + SteeringAngle), -SteeringAngleMax, SteeringAngleMax);
 	}
 	else
 	{
@@ -359,9 +570,55 @@ void AImprezza::Steer(float Value)
 	//UE_LOG(LogTemp, Warning, TEXT("Steer %f"), SteeringAngle);
 	TopLinkFL->SetRelativeRotation(FRotator(0.f, SteeringAngle, 0.f));
 	TopLinkFR->SetRelativeRotation(FRotator(0.f, SteeringAngle, 0.f));
-	SteeringWheel->SetRelativeRotation(FRotator(-20.f, 0.f, (SteeringAngle/SteeringAngleMax)*180.f));
+	SteeringWheel->SetRelativeRotation(FRotator(-20.f, 0.f, (SteeringAngle / SteeringAngleMax)*180.f));
 
-	
+
 	//UE_LOG(LogTemp, Warning, TEXT("%s"), *ApplyingForce.ToString());
 
+}
+
+void AImprezza::SetBodyVisibility()
+{
+	Body->SetVisibility(DebugForces);
+	if (DebugForces == false)
+	{
+		DebugForces = true;
+	}
+	else
+	{
+		DebugForces = false;
+	}
+}
+
+void AImprezza::GearUp()
+{
+	GearTarget = FMath::Min((Gear + 1), (GearRatio.Num() - 1));
+	Gear = 1;
+	TotalGearRatio = 0;
+
+	FTimerHandle Timer;
+	GetWorld()->GetTimerManager().SetTimer(Timer, this, &AImprezza::OnUPTimerExpire, GearChangeTime, false);
+}
+
+void  AImprezza::OnUPTimerExpire()
+{
+	Gear = GearTarget;
+	TotalGearRatio = MainGear * GearRatio[Gear];
+
+}
+
+void AImprezza::GearDown()
+{
+	GearTarget = FMath::Max((Gear - 1), 0);
+	Gear = 1;
+	TotalGearRatio = 0;
+
+	FTimerHandle Timer;
+	GetWorld()->GetTimerManager().SetTimer(Timer, this, &AImprezza::OnDOWNTimerExpire, GearChangeTime, false);
+}
+
+void  AImprezza::OnDOWNTimerExpire()
+{
+	Gear = GearTarget;
+	TotalGearRatio = MainGear * GearRatio[Gear];
 }
--- MyProject2/Source/MyProject2/Public/Imprezza.h
+++ MyProject2/Source/MyProject2/Public/Imprezza.h
@@ -9,22 +9,29 @@
 class UCameraComponent;
 class USpringArmComponent;
 
+UENUM()
+enum class EDriveType : uint8
+{
+	FWD, 
+	RWD,
+	AWD
+};
+
 USTRUCT()
 struct FWheelStruct
 {
 	GENERATED_BODY()
 
 	FWheelStruct()
-	:Radius(34.f),Mass(15.f){}
+	:Radius(34.f), Mass(15.f), CorneringStiffness(1.f), LongStiffness(1.f){}
 
-	FWheelStruct (float inRadius, float inMass)
-	: Radius(inRadius), Mass(inMass){}
+	FWheelStruct(float inRadius, float inMass, float inCornStiff, float inLongStiff)
+	:Radius(inRadius), Mass(inMass), CorneringStiffness(inCornStiff), LongStiffness(inLongStiff){}
 	
-	
-	float Radius ;
-	float Mass ;
-
-	
+	float Radius;
+	float Mass;
+	float CorneringStiffness;
+	float LongStiffness;
 };
 
 USTRUCT()
@@ -32,18 +39,37 @@ struct FSuspensionStruct
 {
 	GENERATED_BODY()
 
-	FSuspensionStruct()
-	:Rest_Length(50.f), Travel(10.f),Stiffness(1500.f), Damper(15.f), Force_Min(-2000.f), Force_Max(6000.f) {}
+		FSuspensionStruct()
+		:Rest_Length(50.f), Travel(10.f), Stiffness(1500.f), Damper(15.f), Force_Min(-2000.f), Force_Max(6000.f) {}
 
-	FSuspensionStruct(float inRestLength, float inTravel,float inStiffness,float inDamper,float inMin, float inMax)
-	: Rest_Length(inRestLength), Travel(inTravel), Stiffness(inStiffness), Damper(inDamper), Force_Min(inMin), Force_Max(inMax){}
+	FSuspensionStruct(float inRestLength, float inTravel, float inStiffness, float inDamper, float inMin, float inMax)
+		: Rest_Length(inRestLength), Travel(inTravel), Stiffness(inStiffness), Damper(inDamper), Force_Min(inMin), Force_Max(inMax) {}
 
-	float Rest_Length ;
+	float Rest_Length;
 	float Travel;
-	float Stiffness ;
+	float Stiffness;
 	float Damper;
 	float Force_Min;
 	float Force_Max;
+
+};
+
+USTRUCT()
+struct FEngineStruct
+{
+	GENERATED_BODY()
+
+		FEngineStruct()
+		:IdleRPM(700.f), MaxRPM(7000.f), Inertia(0.3f), BackTorque(-100.f) {}
+
+	FEngineStruct(float inIdleRPM, float inMaxRPM, float inInertia, float inBackTorque)
+		: IdleRPM(inIdleRPM), MaxRPM(inMaxRPM), Inertia(inInertia), BackTorque(inBackTorque) {}
+
+
+	float IdleRPM;
+	float MaxRPM;
+	float Inertia;
+	float BackTorque;
 
 };
 
@@ -66,6 +92,8 @@ public:
 
 	TArray<FWheelStruct> Wheel;
 
+	FEngineStruct Engine;
+
 	TArray<float> RayLength;
 
 	TArray<bool> WheelContact;
@@ -82,13 +110,89 @@ public:
 
 	TArray<FVector> WheelLinearVelocityLocal;
 
+	TArray<float> GearRatio;
+
+	TArray<float> DriveTorque;
+
+	TArray<float> TorqueRatio;
+
+	TArray<float> WheelInertia;
+
+	TArray<float> WheelAngularVelocity;
+
+	TArray<float> LongSlipVelocity;
+
+	float DeltaTimee;
+
 	float ForwardAxisValue = 0;
 
-	UPROPERTY(EditAnyWhere)
-	float SteeringAngle;
+	float ThrottleValue = 0;
+
+	float EngineRPM = 0;
+
+	float EngineTorque = 0;
+
+	float EngineAngularVelocity = 0;
+
+	float RPM_to_RAD_PS = 0;
+
+	float RAD_PS_to_RPM = 0;
+
+	int32 Gear = 1;
+
+	int32 GearTarget ;
+
+	float MainGear = 3.82;
+
+	float TotalGearRatio ;
+
+	float Efficiency = 0.8;
+
+	float MaxWheelSpeedOnCurrentGear;
+
+	float LateralSlipNormalized = 0.f;
+
+	float LongSlipNormalized = 0.f;
+
+	FVector2D LongLateralSlipVector ;
+
+	float FxVal = 0;
+
+	float FyVal = 0;
+
+	FVector2D TireForce;
+
+	UPROPERTY(EditDefaultsOnly)
+		float GearChangeTime = 0.5f;
+
+	UPROPERTY(EditDefaultsOnly)
+		float FrontBiasPercent = 0.3f;
 
 	UPROPERTY(EditAnyWhere)
-	float SteeringAngleMax = 45.f;
+		bool ThrottleFilter = false;
+
+	UPROPERTY(EditAnyWhere)
+		float SteeringAngle;
+
+	UPROPERTY(EditAnyWhere)
+		float SteeringAngleMax = 45.f;
+
+	UPROPERTY(EditAnyWhere)
+		bool DebugForces = false;
+
+	UPROPERTY(EditAnywhere, Category = "Curve Category")
+		FRuntimeFloatCurve TorqueCurve;
+
+	FKeyHandle TC1;
+	FKeyHandle TC2;
+	FKeyHandle TC3;
+
+	UPROPERTY(EditAnywhere, Category = "Curve Category")
+		FRuntimeFloatCurve ForceCurve;
+
+	FKeyHandle FC1;
+	FKeyHandle FC2;
+	FKeyHandle FC3;
 
 
 protected:
@@ -96,7 +200,7 @@ protected:
 	virtual void BeginPlay() override;
 
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
@@ -104,43 +208,53 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	UStaticMeshComponent* Body = nullptr;
+		UStaticMeshComponent* Body = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	USceneComponent* TopLinkFL = nullptr;
+		USceneComponent* TopLinkFL = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	USceneComponent* TopLinkFR = nullptr;
+		USceneComponent* TopLinkFR = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	USceneComponent* TopLinkRL = nullptr;
+		USceneComponent* TopLinkRL = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	USceneComponent* TopLinkRR = nullptr;
+		USceneComponent* TopLinkRR = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	UStaticMeshComponent* WheelFL = nullptr;
+		UStaticMeshComponent* WheelFL = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	UStaticMeshComponent* WheelFR = nullptr;
+		UStaticMeshComponent* WheelFR = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	UStaticMeshComponent* WheelRL = nullptr;
+		UStaticMeshComponent* WheelRL = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	UStaticMeshComponent* WheelRR = nullptr;
+		UStaticMeshComponent* WheelRR = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	UStaticMeshComponent* SteeringWheel = nullptr;
+		UStaticMeshComponent* SteeringWheel = nullptr;
 
 	UPROPERTY(Category = Camera, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	USpringArmComponent* SpringArm;
-	
+		USpringArmComponent* SpringArm;
+
 	UPROPERTY(Category = Camera, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	UCameraComponent* Camera;
+		UCameraComponent* Camera;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Drive Type")
+		EDriveType DriveType = EDriveType::AWD;
 
 	void LookX(float Value);
 	void LookY(float Value);
 	void Steer(float Value);
+	void Forward(float Value);
 	void Throttle(float Value);
+	void SetBodyVisibility();
+	void GearUp();
+	void GearDown();
+	void OnUPTimerExpire();
+	void OnDOWNTimerExpire();
+
 };
